1e791381b18bc8381441a8603160479dcee23bc0
Ananya Singh <ananya.suhani9415@gmail.com>
2026-05-01 22:53:34 +0530

    Time: 0 ms (100%), Space: 47.8 MB (64.48%) - LeetHub

---
+++ LeetCode/Medium/0050-powx-n/0050-powx-n.java
@@ -0,0 +1,25 @@
+class Solution {
+    public double helper(double x,int n)
+    {
+        if(n==0)
+        return 1;
+        double temp=helper(x,n/2);
+        temp*=temp;
+        if(n%2!=0)
+        temp*=x;
+        return temp;
+
+    }
+    public double myPow(double x, int n) {
+        if(n<0)
+        {
+         long val=(long)n;
+        double ans=helper(x,(int)- val);
+        return 1.0/ans;
+        }
+        return helper(x,n);
+
+
+        
+    }
+}
