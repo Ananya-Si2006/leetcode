673fa2e21cd8e2e3823dca00ecfc079e4a37c2be
Ananya Singh <ananya.suhani9415@gmail.com>
2026-05-20 23:54:04 +0530

    Time: 1 ms (42.1%), Space: 44.8 MB (61.53%) - LeetHub

--- LeetCode/Easy/1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.java
+++ LeetCode/Easy/1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.java
@@ -2,16 +2,17 @@ class Solution {
     public int getMinDistance(int[] nums, int target, int start) {
        int distance=Integer.MAX_VALUE;
        
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]==target)
-            {
-                distance=Math.min(distance,Math.abs(i-start));
-                
-
-            }
-        }
-        return distance;
+       for(int i=start;i<nums.length;i++)
+       {
+        if(nums[i]==target)
+        distance=Math.min(distance,Math.abs(i-start));
+       }
+       for(int i=start ;i>=0;i--)
+       {
+        if(nums[i]==target)
+        distance=Math.min(distance,Math.abs(i-start));
+       }
+       return distance;
         
     }
 }
