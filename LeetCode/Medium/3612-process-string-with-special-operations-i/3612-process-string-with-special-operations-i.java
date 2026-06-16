9a468cda1f78e614b0cee4bcd2a93fe35bcf781b
Ananya Singh <ananya.suhani9415@gmail.com>
2026-06-16 21:55:21 +0530

    Time: 4 ms (76.47%), Space: 55.6 MB (28.68%) - LeetHub

---
+++ LeetCode/Medium/3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(char c: s.toCharArray())
+        {
+            if(Character.isLowerCase(c))
+            {
+                sb.append(c);
+            }
+            else if(c=='*')
+            {
+                if(sb.length()>0)
+                sb.deleteCharAt(sb.length()-1);
+
+            }
+            else if(c=='#')
+            {
+                String s1=sb.toString();
+                sb.append(s1);
+            }
+            else 
+            {
+                sb.reverse();
+
+            }
+        }
+        return sb.toString();
+        
+    }
+}
