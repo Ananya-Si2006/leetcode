ee0efb9a711f0c020e893d57b36c8a91fcd21b5a
Ananya Singh <ananya.suhani9415@gmail.com>
2026-06-21 23:53:00 +0530

    Time: 38 ms (24.21%), Space: 84 MB (43.59%) - LeetHub

---
+++ LeetCode/Medium/1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int count=0;
+        for(int cost:costs)
+        {
+            if(cost<=coins)
+            {
+                count++;
+                coins-=cost;
+            }
+
+        }
+        return count;
+        
+    }
+}
