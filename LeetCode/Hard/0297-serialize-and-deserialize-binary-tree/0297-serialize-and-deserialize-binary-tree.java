9fcbcab2f981739b94090b81bf05dfb3f69afe92
Ananya Singh <ananya.suhani9415@gmail.com>
2026-06-26 23:57:31 +0530

    Time: 7 ms (97.5%), Space: 49.1 MB (5.87%) - LeetHub

---
+++ LeetCode/Hard/0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Serialize
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        preorder(root, sb);
+        return sb.toString();
+    }
+
+    private void preorder(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            sb.append("null,");
+            return;
+        }
+
+        sb.append(node.val).append(",");
+
+        preorder(node.left, sb);
+        preorder(node.right, sb);
+    }
+
+    // Pointer used during deserialization
+    private int index;
+
+    // Deserialize
+    public TreeNode deserialize(String data) {
+
+        String[] values = data.split(",");
+        index = 0;
+
+        return build(values);
+    }
+
+    private TreeNode build(String[] values) {
+
+        if (values[index].equals("null")) {
+            index++;
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.parseInt(values[index]));
+        index++;
+
+        root.left = build(values);
+        root.right = build(values);
+
+        return root;
+    }
+}
