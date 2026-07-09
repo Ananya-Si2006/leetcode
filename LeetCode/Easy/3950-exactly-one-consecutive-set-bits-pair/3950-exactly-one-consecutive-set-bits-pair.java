ead02806e2fca6ff2901c2d818f78f61b7350fd3
Ananya Singh <ananya.suhani9415@gmail.com>
2026-07-09 23:04:40 +0530

    Time: 1 ms (99.07%), Space: 42.4 MB (92.53%) - LeetHub

---
+++ LeetCode/Easy/3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean consecutiveSetBits(int n) {
+        int count=0;
+        while(n>0)
+            {
+                if((n&3)==3)
+                    count++;
+                n=n>>1;
+            }
+        if(count==1)
+            return true;
+        else 
+            return false;
+        
+        
+    }
+}
