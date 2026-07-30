5fdf0eaee24517cf1ab995c93934b35fabce1945
Ananya Singh <ananya.suhani9415@gmail.com>
2026-07-30 23:04:54 +0530

    Time: 3 ms (73.94%), Space: 69.6 MB (44.78%) - LeetHub

--- LeetCode/Easy/0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ LeetCode/Easy/0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,14 +1,14 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        long sum=0;
+        int sum=0;
+        int maxSum=0;
         
         for(int i=0;i<k;i++)
         {
-
             sum+=nums[i];
 
         }
-        long maxSum=sum;
+        maxSum=sum;
         for(int i=k;i<nums.length;i++)
         {
             sum+=nums[i];
@@ -16,5 +16,6 @@ class Solution {
             maxSum=Math.max(maxSum,sum);
         }
         return (double)maxSum/k;
+        
     }
 }
