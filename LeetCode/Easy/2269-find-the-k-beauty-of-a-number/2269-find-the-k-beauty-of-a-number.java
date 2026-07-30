66b409235649508656322ee770379ae66e9356b0
Ananya Singh <ananya.suhani9415@gmail.com>
2026-07-31 00:21:18 +0530

    Time: 1 ms (92.36%), Space: 42.4 MB (36.67%) - LeetHub

---
+++ LeetCode/Easy/2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int divisorSubstrings(int num, int k) {
+        int beauty=0;
+        String s= String.valueOf(num);
+        for(int i=0;i<=s.length()-k;i++)
+        {
+            int val=Integer.parseInt(s.substring(i,i+k));
+            if(val>0 && num%val==0)
+            beauty++;
+        }
+        return beauty;
+        
+    }
+}
