3dc2ab7ad020b625b2b70534ad6c6e63c3c44dde
Ananya Singh <ananya.suhani9415@gmail.com>
2026-08-13 20:45:51 +0530

    Time: 0 ms (100%), Space: 47.1 MB (27.89%) - LeetHub

--- LeetCode/Easy/0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ LeetCode/Easy/0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,23 +14,19 @@
  * }
  */
 class Solution {
-    int diameter = 0;
-
+    int d=0;
     public int diameterOfBinaryTree(TreeNode root) {
+        
         height(root);
-        return diameter;
+        return d;
     }
-
-    private int height(TreeNode node) {
-        if (node == null) {
-            return 0;
-        }
-
-        int left = height(node.left);
-        int right = height(node.right);
-
-        diameter = Math.max(diameter, left + right);
-
-        return 1 + Math.max(left, right);
+    private int height(TreeNode root)
+    {
+        if(root==null)
+        return 0;
+        int left=height(root.left);
+        int right=height(root.right);
+        d=Math.max(d,left+right);
+        return 1+Math.max(left,right);
     }
 }
