cd261f5d6389a5f7f99c4729aa8cb51e6b2aa474
Ananya Singh <ananya.suhani9415@gmail.com>
2026-09-06 19:44:27 +0530

    Time: 3 ms (59.33%), Space: 47.1 MB (40.91%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.java
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.java
@@ -3,12 +3,13 @@ class Solution {
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++)
         {
-            int diff= target-nums[i];
+            int diff=target-nums[i];
             if(map.containsKey(diff))
-            return new int []{map.get(diff),i};
+            return  new int[] {map.get(diff),i};
+            else
             map.put(nums[i],i);
         }
-        return new int []{-1,-1};
+        return new int[] {-1,-1};
         
     }
 }
