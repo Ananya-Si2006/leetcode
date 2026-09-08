6eed155348f5a7d13f538bbb6b85c52c4d6d8c14
Ananya Singh <ananya.suhani9415@gmail.com>
2026-09-08 21:54:01 +0530

    Time: 1 ms (83.65%), Space: 42.5 MB (72.85%) - LeetHub

--- LeetCode/Easy/3870-count-commas-in-range/3870-count-commas-in-range.java
+++ LeetCode/Easy/3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,12 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-        int ans=0;
-        for(int i=1;i<=n;i++)
-        {
-            if(i>=1000)
-            ans++;
-        }
-        return ans;
         
+        if(n<1000)
+        return 0;
+        return n-999;
     }
 }
