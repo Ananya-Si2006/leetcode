af93a8c1275e059b49e39c9832338220a5a25425
Ananya Singh <ananya.suhani9415@gmail.com>
2026-09-13 17:10:13 +0530

    Time: 1 ms (56.17%), Space: 47.9 MB (42.78%) - LeetHub

---
+++ LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int trap(int[] height) {
+        int n=height.length;
+        int lmax=height[0];
+        int rmax=height[n-1];
+        int low=1;
+        int high=n-2;
+        int ans=0;
+        while(low<=high)
+        {
+            lmax=Math.max(lmax,height[low]);
+            rmax=Math.max(rmax,height[high]);
+            if(lmax<=rmax)
+            {
+                ans+=lmax-height[low];
+                low++;
+            }
+            else
+            {
+                ans+=rmax-height[high];
+                high--;
+            }
+        }
+        return ans;
+        
+    }
+}
